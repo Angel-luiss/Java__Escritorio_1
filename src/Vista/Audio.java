de6668dc2d6e7610b036a820947d3a4ed0c27fc7
Angel-luiss <ltucubala@miumg.edu.gt>
2022-08-07 22:23:20 -0600

    primer_commit

---
+++ src/Vista/Audio.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package Vista;
+
+/**
+ *
+ * @author angeltucubal
+ */
+public class Audio {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+        // TODO code application logic here
+    Audio repro = new Audio();
+    repro.setVisible(true);
+    
+    
+}     
+
+    private void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}    
+
+
+
+
+/*package Vista;
+
+/**
+ *
+ * @author angeltucubal
+ */
+//public class Audio {
+    
+  //  Audio repro = new Audio();
+    //repro.setVisible(true);
+    
+//}*/
